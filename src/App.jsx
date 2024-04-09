66be3f1d5506c2d8d378905bacfb06d88f16fe78
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-04-09 11:03:34 +1000

    About me component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import mainImg from "./assets/423619648_3603663076543893_2299142157477955626_n-removebg-preview.png";
 import projectImg from "./assets/project.png";
+import Aboutme from "./components/Aboutme.jsx";
 import Banner from "./components/Banner.jsx";
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
@@ -22,51 +23,7 @@ function App() {
         <Banner />
         <Skills />
         <Project projectImg={projectImg} />
-
-        {/* <!-- about me  --> */}
-        <section className="about" id="about">
-          <h2 className="title">
-            <span>About Me</span>
-          </h2>
-          <div className="flex container about-content">
-            <div className="flex myImg">
-              <img src={mainImg} alt="Bikal" />
-            </div>
-            <div className="my-bio container">
-              <h2 className="joti-one-regular">Bikal Adhikari</h2>
-              <p>
-                With a strong ambition to achieve my career goal as a Software
-                Developer/Engineer, I am dedicated to continually expanding my
-                knowledge of technologies and their foundations. Graduating from
-                University with a Bachelor of Information Technology (Software
-                Development), I have established a solid foundation in
-                programming skills. Through self-directed learning and project
-                development, I have obtained professional experience, including
-                my recent position as a Software Engineer.
-              </p>
-              <p>
-                I am now actively seeking an opportunity to contribute to an
-                exciting business that aims to positively impact millions of
-                Australians by revolutionising the way we serve people. With my
-                expertise in programming languages such as React, JavaScript,
-                and Java, I am eager to join a collaborative and supportive
-                team, building complex solutions from scratch in a fast-paced
-                environment. My goal is to leverage my skills and passion for
-                innovation to drive business growth while further advancing my
-                own career within a secure and autonomous setting.
-              </p>
-              <p>Sydney, Australia</p>
-              <div>
-                <div className="tag">Interest</div>
-                <div className="flex">
-                  <span>Coding</span>
-                  <span>Football</span>
-                  <span>Movies</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <Aboutme mainImg={mainImg} />
 
         <section className="container contact" id="contact">
           <h2 className="title">
---
+++ src/components/Aboutme.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Aboutme = ({ mainImg }) => {
+  return (
+    <section className="about" id="about">
+      <h2 className="title">
+        <span>About Me</span>
+      </h2>
+      <div className="flex container about-content">
+        <div className="flex myImg">
+          <img src={mainImg} alt="Bikal" />
+        </div>
+        <div className="my-bio container">
+          <h2 className="joti-one-regular">Bikal Adhikari</h2>
+          <p>
+            With a strong ambition to achieve my career goal as a Software
+            Developer/Engineer, I am dedicated to continually expanding my
+            knowledge of technologies and their foundations. Graduating from
+            University with a Bachelor of Information Technology (Software
+            Development), I have established a solid foundation in programming
+            skills. Through self-directed learning and project development, I
+            have obtained professional experience, including my recent position
+            as a Software Engineer.
+          </p>
+          <p>
+            I am now actively seeking an opportunity to contribute to an
+            exciting business that aims to positively impact millions of
+            Australians by revolutionising the way we serve people. With my
+            expertise in programming languages such as React, JavaScript, and
+            Java, I am eager to join a collaborative and supportive team,
+            building complex solutions from scratch in a fast-paced environment.
+            My goal is to leverage my skills and passion for innovation to drive
+            business growth while further advancing my own career within a
+            secure and autonomous setting.
+          </p>
+          <p>Sydney, Australia</p>
+          <div>
+            <div className="tag">Interest</div>
+            <div className="flex">
+              <span>Coding</span>
+              <span>Football</span>
+              <span>Movies</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Aboutme;
